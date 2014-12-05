5dd18450a79c2347f6eaa56bbe691c390fc1f70d
Victor Pereira <vpereira@suse.de>
2014-12-05 19:57:59 +0100

    added votos to cargo through candidatos

--- app/models/cargo.rb
+++ app/models/cargo.rb
@@ -1,4 +1,5 @@
 class Cargo < ActiveRecord::Base
     has_many :candidatos,:foreign_key=>"numero_cargo"  ,:primary_key=>"numero"
     belongs_to :eleicao, :foreign_key=>"numero_eleicao",:primary_key=>"numero"
+    has_many :votos, :through=> :candidatos
 end
